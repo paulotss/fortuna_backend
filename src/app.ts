0c05981d37516a9c2be2476a22ae6ad0fcf3cb3f
Paulo de Tarso <paulo.oinab@gmail.com>
2023-09-21 12:15:24 -0300

    install cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import clientRouter from './routes/client.route'
 import sellerRouter from './routes/seller.route'
 import ErrorHandle from './middlewares/ErrorHandle'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(clientRouter)
 app.use(sellerRouter)
